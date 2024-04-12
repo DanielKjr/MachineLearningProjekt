3d4b5cb7cad9b74ec32b7bb180333fa484cede3c
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2024-04-12 10:08:13 +0200

    Manual gridsearch med plots

--- Pokemonstuff/ManualGridSearch.py
+++ Pokemonstuff/ManualGridSearch.py
@@ -1,11 +1,10 @@
 import os
 import tensorflow as tf
-
+import matplotlib.pyplot as plt
 
 # Define hyperparameters to experiment with
 learning_rates = [0.001, 0.01, 0.1]
 batch_sizes = [16, 32, 64]
-# batch_sizes = [1]
 num_epochs = [3, 5, 8]
 # num_epochs = [1]
 optimizers = ['adam', 'sgd', 'rmsprop']
@@ -33,62 +32,78 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
     batch_size=32
 )
 
+all_figures = []
+
 for optimizer in optimizers:
 
-    # Perform manual hyperparameter search
-    for lr in learning_rates:
-        for bs in batch_sizes:
-            for epochs in num_epochs:
-                print(f"Training model with lr={lr}, batch_size={bs}, epochs={epochs}, optimizer={optimizer}")
+    # Create a subplot for each optimizer
+    fig, axs = plt.subplots(len(learning_rates), len(batch_sizes), figsize=(15, 15))
+    fig.suptitle(f'Optimizer: {optimizer}')
 
-                # Define and compile the model
-                model = tf.keras.Sequential([
-                    tf.keras.layers.Rescaling(1. / 255),
-                    tf.keras.layers.Conv2D(32, 3, activation='relu'),
-                    tf.keras.layers.MaxPooling2D(),
-                    tf.keras.layers.Conv2D(32, 3, activation='relu'),
-                    tf.keras.layers.MaxPooling2D(),
-                    tf.keras.layers.Conv2D(32, 3, activation='relu'),
-                    tf.keras.layers.MaxPooling2D(),
-                    tf.keras.layers.Flatten(),
-                    tf.keras.layers.Dense(128, activation='relu'),
-                    tf.keras.layers.Dense(973, activation='softmax')
-                ])
+    for i, lr in enumerate(learning_rates):
+        for j, bs in enumerate(batch_sizes):
 
-                if optimizer == 'adam':
-                    optimizer_instance = tf.keras.optimizers.Adam(learning_rate=lr)
-                elif optimizer == 'sgd':
-                    optimizer_instance = tf.keras.optimizers.SGD(learning_rate=lr)
-                elif optimizer == 'rmsprop':
-                    optimizer_instance = tf.keras.optimizers.RMSprop(learning_rate=lr)
-                else:
-                    raise ValueError(f"Unknown optimizer: {optimizer}")
+            # Define and compile the model
+            model = tf.keras.Sequential([
+                tf.keras.layers.Rescaling(1. / 255),
+                tf.keras.layers.Conv2D(32, 3, activation='relu'),
+                tf.keras.layers.MaxPooling2D(),
+                tf.keras.layers.Conv2D(32, 3, activation='relu'),
+                tf.keras.layers.MaxPooling2D(),
+                tf.keras.layers.Conv2D(32, 3, activation='relu'),
+                tf.keras.layers.MaxPooling2D(),
+                tf.keras.layers.Flatten(),
+                tf.keras.layers.Dense(128, activation='relu'),
+                tf.keras.layers.Dense(973, activation='softmax')
+            ])
 
-                model.compile(
-                    optimizer=optimizer_instance,
-                    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                    metrics=['accuracy']
-                )
+            if optimizer == 'adam':
+                optimizer_instance = tf.keras.optimizers.Adam(learning_rate=lr)
+            elif optimizer == 'sgd':
+                optimizer_instance = tf.keras.optimizers.SGD(learning_rate=lr)
+            elif optimizer == 'rmsprop':
+                optimizer_instance = tf.keras.optimizers.RMSprop(learning_rate=lr)
+            else:
+                raise ValueError(f"Unknown optimizer: {optimizer}")
 
-                # Train the model
-                history = model.fit(
-                    train_ds,
-                    validation_data=val_ds,
-                    epochs=epochs,
-                    batch_size=bs,
-                    verbose=0
-                )
+            model.compile(
+                optimizer=optimizer_instance,
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy']
+            )
 
-                # Evaluate model performance
-                _, accuracy = model.evaluate(val_ds, verbose=0)
+            # Train the model
+            history = model.fit(
+                train_ds,
+                validation_data=val_ds,
+                epochs=num_epochs[0],  # Choose the first value in num_epochs for simplicity
+                batch_size=bs,
+                verbose=0
+            )
 
-                # Update best hyperparameters if necessary
-                if accuracy > best_accuracy:
-                    best_accuracy = accuracy
-                    best_hyperparameters = {'learning_rate': lr, 'batch_size': bs, 'epochs': epochs,
-                                            'optimizer': optimizer}
+            # Evaluate model performance
+            _, accuracy = model.evaluate(val_ds, verbose=0)
 
+            # Update best hyperparameters if necessary
+            if accuracy > best_accuracy:
+                best_accuracy = accuracy
+                best_hyperparameters = {'learning_rate': lr, 'batch_size': bs, 'optimizer': optimizer}
 
+            # Plot accuracy and loss
+            axs[i, j].plot(history.history['accuracy'], label='accuracy')
+            axs[i, j].plot(history.history['val_accuracy'], label='val_accuracy')
+            axs[i, j].plot(history.history['loss'], label='loss')
+            axs[i, j].plot(history.history['val_loss'], label='val_loss')
+            axs[i, j].set_title(f'lr={lr}, batch_size={bs}')
+            axs[i, j].set_xlabel('Epoch')
+            axs[i, j].set_ylabel('Metric')
+            axs[i, j].legend()
+
+    # Add the figure to the list of all figures
+    all_figures.append(fig)
+
+# Show all figures together
+plt.show()
 
 print("Best hyperparameters:", best_hyperparameters)
 print("Best accuracy:", best_accuracy)
